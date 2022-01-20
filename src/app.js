44735fa3341b2933a4e90d175e9e52b7ee07152d
Nicolas Garces <nicolasgarces0@gmail.com>
2022-01-20 03:52:08 +0000

    Agregue la actividad

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generadorPalos() {
+  let palos = ["♦", "♥", "♠", "♣"];
+  let palosAleatorio = Math.floor(Math.random() * palos.length);
+  return palos[palosAleatorio];
+}
+console.log(generadorPalos());
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generadorNumeros() {
+  let numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  let numerosAleatorio = Math.floor(Math.random() * numeros.length);
+  return numeros[numerosAleatorio];
+}
+console.log(generadorNumeros());
+
+function inyectaNumero() {
+  document.getElementById("numero").innerHTML = generadorNumeros();
+}
+
+function inyectaPalos() {
+  let paloElegido = generadorPalos();
+  document.getElementById("paloArriba").innerHTML = paloElegido;
+  document.getElementById("paloAbajo").innerHTML = paloElegido;
+
+  let paloArriba = document.getElementById("paloArriba");
+  let paloAbajo = document.getElementById("paloAbajo");
+  let numero = document.getElementById("numero");
+  let colornumero = Math.floor(Math.random() * 13);
+  let colores = Math.floor(Math.random() * 2);
+  if (
+    paloElegido === "♦" ||
+    paloElegido === "♥" ||
+    numero === "♦" ||
+    numero === "♥"
+  ) {
+    paloArriba.style.color = "red";
+    paloAbajo.style.color = "red";
+    numero.style.color = "red";
+  }
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generadorPalos();
+  generadorNumeros();
+  inyectaNumero();
+  inyectaPalos();
 };
